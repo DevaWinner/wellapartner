76cfb1f288b374c68352acd8e769b9e981038ed8
DevaWinner <winneraaniekan@gmail.com>
2023-12-22 03:19:47 +0100

    Add Pricing page component

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,15 @@
+import PricingComponent from "../components/PricingComponent";
+import PlanComparison from "../components/PlanComparison";
+import FAQComponent from "../components/FAQComponent";
+
+const PricingPage = () => {
+	return (
+		<div>
+			<PricingComponent />
+			<PlanComparison />
+      <FAQComponent />
+		</div>
+	);
+};
+
+export default PricingPage;
